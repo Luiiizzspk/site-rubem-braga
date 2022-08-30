2d6e0df9718fe384266fce6c3ec209c4def425fe
Luiiizzspk <brluizhd@gmail.com>
2022-08-30 11:53:14 -0300

    Adicionando JS

--- js/code.js
+++ js/code.js
@@ -1,2 +1,39 @@
 //Códigos para o Hot site Rubem Braga   
 
+// Cria um novo objeto da classe Date
+const tempo = new Date()
+const txtData = tempo.getFullYear() + ' - hora: ' + tempo.getHours() + ' minuto: ' + tempo.getMinutes()
+
+// Elementos DOM
+const spanData = document.getElementById('data')
+const btFechar = document.getElementById('btFechar')
+const divModal = document.getElementById('divModal')
+const navPhone = document.getElementById('navPhone')
+const btHamburguer = document.getElementById('btHamburguer')
+const idX = document.getElementById('idX')
+
+// Insere txtData dentro do ID "data"
+spanData.innerText = txtData
+
+
+// Ações do Usuario (eventos)
+
+// Ao clicar no id "btFechar"
+btFechar.addEventListener('click', function(){
+    // oculta o divModal - CSS display:none por JavaScript
+    divModal.style.display = 'none'
+})
+    
+btHamburguer.addEventListener('click', function(){
+    navPhone.style.display = 'block'
+
+})
+
+idX.addEventListener('click', function(){
+ navPhone.style.display = 'none'
+})
+    
+
+
+
+
